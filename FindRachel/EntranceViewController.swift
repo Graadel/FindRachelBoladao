e51b3e18e9e0e202c8afa48622d2126e662a7cde
jmbaitelli <jmbaitelli@gmail.com>
2015-06-12 16:39:36 -0300

    Tela de entrada
    Primeira tela do jogo <3

---
+++ FindRachel/EntranceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EntranceViewController.swift
+//  FindRachel
+//
+//  Created by Julia Motta Baitelli on 12/06/15.
+//  Copyright (c) 2015 Julia Motta Baitelli. All rights reserved.
+//
+
+import UIKit
+
+class EntranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
